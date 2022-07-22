ec199d189b7d787107ee0caa43179800f7dcb65a
mariem fathallah <mariemfathallah737@gmail.com>
2022-07-22 05:35:17 -1200

    first commit

---
+++ calc.js
@@ -0,0 +1,14 @@
+
+   exports.add = (a,b)=>console.log('a+b = ' + (a+b))
+      
+   exports.sub = (a,b)=> console.log('a-b = ' + (a-b))
+
+  exports.mult = (a,b)=> console.log('a* b = ' + (a*b))
+    
+  exports.div = (a,b)=> console.log('a/b = ' + (a/b))
+      
+
+
+
+
+                                    
